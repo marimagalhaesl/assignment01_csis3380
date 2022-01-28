6831af737a446c14ddc9b90f828b48cc875d8f62
Mariana Magalhães <marianamagalhaes@Marianas-MacBook-Pro.local>
2022-01-27 17:08:32 -0800

    added quotes

--- public/js/script.js
+++ public/js/script.js
@@ -4,8 +4,38 @@
 /*** 
  * `quotes` array 
 ***/
+const quotes = [
+    {
+        quote: "Do not pity the dead, Harry. Pity the living, and, above all those who live without love",
+        source: "– Albus Dumbledore, Harry Potter and the Deathly Hallows, Chapter 35"
+    },
+    {
+        quote: "It is impossible to manufacture or imitate love",
+        source: "– Horace Slughorn, Harry Potter and the Half-Blood Prince, Chapter 9"
+    },
+    {
+        quote: "Love as powerful as your mother’s for you leaves it’s own mark. To have been loved so deeply, even though the person who loved us is gone, will give us some protection forever.",
+        source: "― Albus Dumbledore, Harry Potter and the Sorcerer’s Stone, Chapter 17"
+    },
+    {
+        quote: "It takes a great deal of bravery to stand up to our enemies, but just as much to stand up to our friends.",
+        source: "– Albus Dumbledore, Harry Potter and the Sorcerer’s Stone, Chapter 17"
+    },
+    {
+        quote: "Numbing the pain for a while will make it worse when you finally feel it.",
+        source: "– Albus Dumbledore, Harry Potter and the Goblet of Fire, Chapter 36"
+    },
+    {
+        quote: "If you want to know what a man’s like, take a good look at how he treats his inferiors, not his equals.",
+        source: "– Sirius Black, Harry Potter and the Goblet of Fire, Chapter 27"
+    },
+    {
+        quote: "I am what I am, an’ I’m not ashamed. ‘Never be ashamed,’ my ol’ dad used ter say, ‘there’s some who’ll hold it against you, but they’re not worth botherin’ with.",
+        source: "- Rubeus Hagrid, Harry Potter and the Goblet of Fire, Chapter 24"
+    }
+]
 
-
+quotes[0].quote
 
 /***
  * `getRandomQuote` function
